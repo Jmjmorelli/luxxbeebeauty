75b552f0481a4530413e280ee12f28caa64d9cb6
Jmjmorelli <Jvgmorelli@gmail.com>
2026-06-09 16:46:14 -0400

    import ui components

---
+++ luxbeauty/app/data/users.ts
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    id: "1",
+    name: "Arham Khan",
+    username: "Aarhamkhnz",
+    email: "hello@arhamkhnz.com",
+    avatar: "https://avatars.githubusercontent.com/u/43849669",
+    role: "administrator",
+  },
+//   {
+//     id: "2",
+//     name: "joao",
+//     username: "joaozinhoebonito",
+//     email: "1@1.com",
+//     avatar: "",
+//     role: "admin",
+//   },
+];
+
+export const rootUser = users[0];
